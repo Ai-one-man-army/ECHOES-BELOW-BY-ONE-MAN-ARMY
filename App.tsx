f67c02326df92245ad54bcebb54d2d50a5d81277
Misbaul Alam <132201880+MISBAUL1200@users.noreply.github.com>
2026-08-30 01:14:23 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import { HUD } from "./HUD";
 import { MobileControls } from "./MobileControls";
 import { PauseMenu } from "./PauseMenu";
 import { Sector9Modal } from "./Sector9Modal";
-import GameOverModal from "./GameOverModal";
+import { GameOverModal } from "./GameOverModal";
 import { CreditsScreen } from "./CreditsScreen";
 
 
